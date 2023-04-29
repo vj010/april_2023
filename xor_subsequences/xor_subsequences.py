397c9b570b6ea83ec8f14eb97bb0f1b60d5b8846
vijay <vjblacklight@gmail.com>
2023-04-29 09:02:56 +0530

    ADD - accepted solution of xor_subsequences

---
+++ xor_subsequences/xor_subsequences.py
@@ -0,0 +1,19 @@
+import sys
+import numpy as np
+sys.stdin = open('./xor_subsequences/input.txt', 'r')
+sys.stdout = open('./xor_subsequences/output.txt', 'w')
+n = int(input().strip())
+arr = np.array(list(map(int, input().strip().split(' '))))
+xors = set()
+ans = []
+for x in arr:
+    tmp_set = set()
+    for v in xors:
+        new_xor = v ^ x
+        if new_xor not in xors:
+            tmp_set.add(new_xor)
+    if x not in xors:
+        xors.add(x)
+    xors = xors.union(tmp_set)
+    ans.append(len(xors))
+print(' '.join(str(a) for a in ans))
